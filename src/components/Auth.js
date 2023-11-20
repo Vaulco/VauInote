fa74fe712d83b9814465c89e9e606589e122d609
DanyilNiemtsov <dniemtsov@gmail.com>
2023-11-20 16:23:51 +1300

    added logo and changed login styles

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,6 +2,7 @@ import { auth, provider } from '../firebase-config.js';
 import { signInWithPopup } from 'firebase/auth'
 import Cookies from 'universal-cookie';
 import '../App.css'
+import logo from './assets/Untitled.png'
 
 const cookies = new Cookies()
 
@@ -22,7 +23,12 @@ export const Auth = (props) => {
   return ( 
     <div className="w-full absolute h-full flex justify-center items-center bg-[#141517] font-[poppins]">
       <header className='fixed top-0 left-0 w-full h-[50px] bg-[#191a1c] flex items-center'>
-      <button className='w-[5rem] h-60px bg-[#5a5d62] p-[5px] rounded-[8px] absolute right-2 font-semibold text-[#36383f] hover:bg-[#5f6162] duration-300 hover:text-[#333539]' onClick={signInWithGoogle}> Login</button>
+      <img className='absolute w-[30px] left-[0.5rem]' src={logo} alt=''/>
+      <h1 className='font-semibold text-[#bfc2c5] absolute left-[3rem] text-[25px]'>Vaulnote</h1>
+
+      
+
+      <button className='w-[5rem] h-60px bg-[#1d1e1f] p-[5px] rounded-[8px] absolute right-2 font-semibold text-[#919495] duration-300 border-[2px] border-[#292a2c]' onClick={signInWithGoogle}>Login</button>
       </header>
     </div>
   )

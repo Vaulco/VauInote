d4582270175d62a815328532c585f47822554bfe
DanyilNiemtsov <dniemtsov@gmail.com>
2023-11-23 14:07:33 +1300

    added save boxes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
+import { collection, addDoc, getDocs, where, query } from 'firebase/firestore';
+import { db } from './firebase-config'; // Make sure to import your Firestore configuration
 import Home from './components/assets/home.svg';
-import Search from './components/assets/search.svg';
+import chat from './components/assets/chat.svg'
 import Logs from './components/assets/logout.svg';
-import menus from './components/assets/menu.svg';
-import React, { useState, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Auth } from "./components/Auth";
 import Cookies from 'universal-cookie';
 import { Chat } from "./components/Chat";
-import { signOut } from 'firebase/auth';
-import { auth } from './firebase-config';;
-
+import { signOut, getAuth, onAuthStateChanged, } from 'firebase/auth';
 
 const cookies = new Cookies();
 
@@ -19,6 +18,63 @@ const App = () => {
   const [showChat, setShowChat] = useState(false);
   const roomInputRef = useRef(null);
   const [menuActive, setMenuActive] = useState(false);
+  const [savedRooms, setSavedRooms] = useState([]);
+  const auth = getAuth();
+
+  useEffect(() => {
+    const loadSavedRooms = async () => {
+      if (auth.currentUser) {
+        try {
+          const q = query(
+            collection(db, 'savedRooms'),
+            where('uid', '==', auth.currentUser.uid)
+          );
+          const querySnapshot = await getDocs(q);
+          const loadedRooms = [];
+          querySnapshot.forEach((doc) => {
+            loadedRooms.push(doc.data().room);
+          });
+          setSavedRooms(loadedRooms);
+        } catch (error) {
+          console.error('Error loading documents: ', error);
+        }
+      }
+    };
+
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        loadSavedRooms();
+      } else {
+        setSavedRooms([]);
+      }
+    });
+
+    return () => unsubscribe();
+  }, [auth.currentUser]);
+
+  const handleSaveRoom = async () => {
+    if (room && !savedRooms.includes(room) && auth.currentUser) {
+      const newSavedRooms = [...savedRooms, room];
+      setSavedRooms(newSavedRooms);
+
+      try {
+        await addDoc(collection(db, 'savedRooms'), {
+          uid: auth.currentUser.uid,
+          displayName: auth.currentUser.displayName,
+          email: auth.currentUser.email,
+          room: room,
+        });
+      } catch (error) {
+        console.error('Error adding document: ', error);
+      }
+    }
+  };
+
+  const handleOpenSavedRoom = (savedRoom) => {
+    setRoom(savedRoom);
+    setInChat(true);
+    setShowChat(true);
+  };
 
   const handleSearch = () => {
     setRoom(roomInputRef.current.value);
@@ -41,6 +97,10 @@ const App = () => {
     setRoom(null);
   };
 
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
   if (!isAuth) {
     return (
       <div>
@@ -49,34 +109,35 @@ const App = () => {
     );
   }
 
-  const toggleMenu = () => {
-    setMenuActive(!menuActive);
-  };
-
   return (
     <>
-      <div className='bg-[#141517] room w-full absolute h-full flex justify-center items-center font-[Poppins]'>
-        <header className={`absolute right-0 top-0 w-[calc(100%-72px)] justify-center flex h-[2.5rem] bg-[#191a1c] border-b-[1px] border-[#292a2c] items-center duration-300 ${menuActive ? ' active-header' : ''}`}>
-        <img onClick={toggleMenu} className='w-[25px] absolute left-2' src={menus} alt='' />
+      <div className='bg-[#1e1f22] room w-full absolute h-full flex justify-center items-center font-[Poppins]'>
+        <header className={`absolute right-0 top-0 w-[calc(100%-72px)] justify-center flex h-[2.5rem] bg-[#1e1f22] items-center duration-300 ${menuActive ? ' active-header' : ''}`}>
+          <i onClick={toggleMenu} className='bx bx-menu text-[#bfc2c5] text-[25px] font-thin w-[25px] absolute left-2'></i>
           <h4 className='text-[#bfc2c5]'>Home</h4>
-        <form className='rounded-[10px] border-[#292a2c] border-[2px] absolute w-[70%] h-[2.4rem] flex justify-center top-[100px]'>
-          <input onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              e.preventDefault();
-              handleSearch();
-            }
-          }} className='absolute w-[100%] p-[10px] top-0 outline-none rounded-[10px] bg-[#1c1e1f] text-[#bfc2c5] placeholder-[#757575] h-[35px] text-[15px]' maxLength={32} ref={roomInputRef} placeholder='search rooms' />
-          <i onClick={handleSearch} className='bx bx-search absolute top-[4px] right-[8px] text-xl font-light text-[#bfc2c5]'></i>
-        </form>
+          <form className='rounded-[10px] border-[#292a2c] border-[2px] absolute w-[70%] h-[2.4rem] flex justify-center top-[100px]'>
+            <input onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                e.preventDefault();
+                handleSearch();
+              }
+            }} className='absolute w-[100%] p-[10px] top-0 outline-none rounded-[10px] bg-[#26272b] text-[#bfc2c5] placeholder-[#757575] h-[35px] text-[15px]' maxLength={32} ref={roomInputRef} placeholder='search rooms' />
+            <i onClick={handleSearch} className='bx bx-search absolute top-[4px] right-[8px] text-xl font-light text-[#bfc2c5]'></i>
+          </form>
         </header>
-        <div className={`w-[72px] h-full absolute bg-[#191a1c] left-0 border-r-[1px] border-[#292a2c] p-[11px] duration-300 ${menuActive ? ' active-form' : ''}`}>
-          <div onClick={handleBackButtonClick} className='w-full h-[47.5px] bg-[#18191B] relative top-0 border-2 border-[#292a2c] rounded-2xl flex justify-center mb-[11px] hover:bg-[#1d1e1f] hover:scale-95 duration-300'>
+        <div className={`w-[72px] h-full absolute bg-[#191a1c] left-0 p-[11px] duration-300 flex flex-col items-center ${menuActive ? ' active-form' : ''}`}>
+          <div onClick={handleBackButtonClick} className='w-full h-[47.5px] bg-[#1e1f22] relative top-0  rounded-xl flex justify-center mb-[11px] hover:scale-95 duration-300 hover:bg-[#26272b] cursor-pointer'>
             <img className='w-[25px]' src={Home} alt=''/>
           </div>
-          <div  onClick={handleSearch} className='w-full h-[47.5px] bg-[#18191B] relative top-0 border-2 border-[#292a2c] rounded-2xl flex justify-center mb-[11px]'>
-            <img className='w-[25px]' src={Search} alt=''/>
+          <div className='w-[25px] h-[1px] bg-[#bfc2c5] rounded-sm'></div>
+          <div className='chat-container w-[49.5px] top-[76.2px] h-[calc(100%-8.77rem)] mt-[5.5px] absolute overflow-y-auto'>
+            {savedRooms.map((savedRoom) => (
+              <div key={savedRoom} className='w-[49.5px] h-[47.5px] bg-[#1e1f22] relative top-0 rounded-xl flex justify-center mb-[11px] hover:scale-95 duration-300 hover:bg-[#26272b] cursor-pointer' onClick={() => handleOpenSavedRoom(savedRoom)}>
+                <img className='w-[24px]' src={chat} alt='' />
+              </div>
+            ))}
           </div>
-          <div onClick={signUserOut} className='w-[49px] h-[47.5px] bg-[#18191B] absolute bottom-0 border-2 border-[#292a2c] rounded-2xl flex justify-center mb-[11px] hover:bg-[#1d1e1f] hover:scale-95 duration-300'>
+          <div onClick={signUserOut} className='w-[49.5px] h-[47.5px] bg-[#1e1f22] absolute bottom-0  rounded-xl flex justify-center mb-[11px] hover:scale-95 duration-300 hover:bg-[#26272b] cursor-pointer'>
             <img className='w-[22px]' src={Logs} alt=''/>
           </div>
         </div>
@@ -84,6 +145,10 @@ const App = () => {
 
       {showChat && (
         <Chat room={room} />
+      )}
+
+      {inChat && (
+          <i onClick={handleSaveRoom} className="bx bxs-save absolute top-2  right-2 text-[22px] text-[#bfc2c5] font-thin cursor-pointer hover:text-white duration-300"></i>
       )}
     </>
   );
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,3 @@
-import menus from './assets/menu.svg';
 import { useEffect, useState } from "react";
 import { addDoc, collection, serverTimestamp, onSnapshot, query, where, orderBy, deleteDoc, doc, updateDoc, } from "firebase/firestore";
 import { auth, db } from "../firebase-config";
@@ -14,7 +13,6 @@ export const Chat = (props) => {
   const messagesRef = collection(db, "messages");
   const [menuActive, setMenuActive] = useState(false);
   
-
   useEffect(() => {
     const queryMessages = query(
       messagesRef,
@@ -105,6 +103,7 @@ export const Chat = (props) => {
         user: auth.currentUser.displayName,
         room,
         userAvatar: auth.currentUser.photoURL, // Added this line
+        email: auth.currentUser.email,
       });
     }
 
@@ -117,18 +116,18 @@ export const Chat = (props) => {
   };
 
   return (
-    <div className={`bg-[#141517] w-full absolute h-full flex justify-center items-center font-[poppins] right-0 duration-300 ${menuActive ? ' active-box' : 'sm:w-[calc(100%-72px)]'}`}>
-      <header className=" w-full h-[2.5rem] absolute top-0 flex justify-center items-center border-[#292a2c] border-b-[1px] bg-[#191a1c]">
-      <img onClick={toggleMenu} className='w-[25px] absolute left-2 z-10 md:hidden' src={menus} alt='' />
+    <div className={`bg-[#1e1f22] w-full absolute h-full flex justify-center items-center font-[poppins] right-0 duration-300 ${menuActive ? ' active-box' : 'sm:w-[calc(100%-72px)]'}`}>
+      <header className=" w-full h-[2.6rem] absolute top-0 flex justify-center items-center border-[#292a2c] border-b-[1px] bg-[#323338c]">
+        <i onClick={toggleMenu} className='bx bx-menu w-[25px] absolute left-2 z-10 md:hidden'></i>
         <h4 className="absolute font-normal text-[#bfc2c5]">{room}</h4>
       </header>
       <div style={{ height: 'calc(100% - 6rem)' }} className={`messages w-full overflow-y-auto p-[0] rounded-[5px] mt-[40px] absolute top-0 left-0 duration-300  ${menuActive ? ' active-menu-wrapper' : 'sm:opacity-100' }`}>
         {messages.map((message) => (
-          <div key={message.id} className="message text-[#dbdee1] bg-[transparent] font-light text-[14px] p-[5px] pt-2 pb-2 mt-[8px] relative border-[transparent] border-t-[2px] border-b-[2px] rounded-[5px] hover:bg-[#18191b] hover:border-[#292a2c] duration-300 break-words">
+          <div key={message.id} className="message text-[#dbdee1] bg-[transparent] font-light text-[14px] p-[5px] pt-2 pb-2 mt-[8px] relative border-[transparent] border-t-[2px] border-b-[2px] rounded-[5px] hover:bg-[#26272b] hover:border-[transparent] duration-300 break-words">
             {auth.currentUser && auth.currentUser.displayName === message.user && (
               <div className="">
-                <i onClick={() => handleDelete(message.id)} className="delete-icon bx bx-trash absolute right-[15px] top-[-10px] text-[#bfc2c5] text-[19px] font- cursor-pointer bg-[#18191b] p-[4px] pl-3 rounded-md border-[#292a2c] border-2 duration-300 opacity-0 hover:text-white"></i>
-                <i onClick={() => handleEdit(message.id)} className="edit-icon bx bxs-pencil absolute right-[40px] top-[-10px] text-[19px] text-[#bfc2c5] cursor-pointer bg-[#18191b] p-[4px] rounded-md rounded-tr-none rounded-br-none border-l-2 border-b-2 border-t-2 border-[#292a2c] duration-300 opacity-0 hover:text-white"></i>
+                <i onClick={() => handleDelete(message.id)} className="delete-icon bx bx-trash absolute right-[15px] top-[-10px] text-[#bfc2c5] text-[19px] font- cursor-pointer bg-[#26272b] p-[4px] pl-3 rounded-md border-[#1e1f22] border-[2px] duration-300 opacity-0 hover:text-white"></i>
+                <i onClick={() => handleEdit(message.id)} className="edit-icon bx bxs-pencil absolute right-[40px] top-[-10px] text-[19px] text-[#bfc2c5] cursor-pointer bg-[#26272b] p-[4px] rounded-md rounded-tr-none rounded-br-none border-l-[2px] border-b-[2px] border-t-[2px] border-[#1e1f22] duration-300 opacity-0 hover:text-white"></i>
               </div>
             )}
             {message.userAvatar && <img src={message.userAvatar} alt="User Avatar" className="avatar w-[36px] rounded-full absolute top-[8px] ml-[0.23rem] sm:ml-[0.7rem]" />}
@@ -157,9 +156,9 @@ export const Chat = (props) => {
           </div>
         ))}
       </div>
-      <form onSubmit={handleSubmit} className="absolute bottom-[8px] w-[70%] flex justify-center shadow-[-1px_-10px_20px_0px_rgba(25,26,28)] h-[3rem] rounded-[10px] bg-[#18191b] border-[#292a2c] border-[1px]">
-        <input onChange={(e) => setNewMessage(e.target.value)} value={newMessage} className="new-message-input placeholder-[#757575] absolute w-[calc(100%-40px)] left-0 p-[10px] bottom-0 rounded-[10px] bg-[#18191b] text-[#bfc2c5] h-[45px] text-[15px] outline-none" placeholder={"Message" + " " + (room)}/>
-        <i type="submit" onClick={handleSubmit} className="send-button bx bxs-send right-[8px] text-[20px] bottom-[7px] text-[#bfc2c5] absolute bg-[#272727] p-[5px] rounded-md hover:text-white duration-300"></i>
+      <form onSubmit={handleSubmit} className="absolute bottom-[8px] w-[70%] flex justify-center h-[45px] rounded-[10px] bg-[#26272b] ">
+        <input onChange={(e) => setNewMessage(e.target.value)} value={newMessage} className="new-message-input placeholder-[#757575] absolute w-[calc(100%-40px)] left-0 p-[10px] bottom-0 rounded-[10px] bg-[#26272b] text-[#bfc2c5] h-[45px] text-[15px] outline-none" placeholder={"Message" + " " + (room)}/>
+        <i type="submit" onClick={handleSubmit} className="send-button bx bxs-send right-[8px] text-[20px] bottom-[7px] text-[#bfc2c5] absolute bg-[transparent] p-[5px] rounded-md hover:text-white duration-300"></i>
       </form>
     </div>
   );
